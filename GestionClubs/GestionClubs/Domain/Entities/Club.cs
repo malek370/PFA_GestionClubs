dd2cb07f061d41ce9a3cd962fe2376f757325210
mbokri <malek.bokri@gmail.com>
2026-04-24 10:59:46 +0100

    corriger tests

---
+++ GestionClubs/GestionClubs/Domain/Entities/Club.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Club:BaseEntity
+    {
+        [Required]
+        public required string Name { get; set; }
+        [Required]
+        public required string Description { get; set; }
+        public Collection<string> Documents { get; set; } = [];
+        
+
+    }
+}
