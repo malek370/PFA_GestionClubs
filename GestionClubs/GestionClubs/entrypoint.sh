7b6cce0694c2ad410f2f87cfd7321af221aea373
mbokri <malek.bokri@gmail.com>
2026-05-14 16:38:40 +0100

    MORE CORRECTION OF PIPELINE

--- GestionClubs/GestionClubs/entrypoint.sh
+++ GestionClubs/GestionClubs/entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 # Trust the self-signed certificate mounted at /https/aspnetapp.crt
 if [ -f /https/aspnetapp.crt ]; then
-    cp /https/aspnetapp.crt /usr/local/share/ca-certificates/aspnetapp.crt
-    update-ca-certificates
+    sudo cp /https/aspnetapp.crt /usr/local/share/ca-certificates/aspnetapp.crt
+    sudo update-ca-certificates
 fi
 
 exec dotnet GestionClubs.API.dll
